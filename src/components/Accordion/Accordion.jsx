6c4eb9a9a2c04055ce0069370d31b23547562dae
Emeric <emeric.pataut@gmail.com>
2025-02-23 19:42:33 +0100

    feat(project): Add project description

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,24 +1,16 @@
-import { useState } from 'react'; // Gere l'état des accordéons ouvert ou fermé
 import PropTypes from 'prop-types';
 import '../Accordion/_accordion.scss';
-import arrowIcon from "../../assets/icons/accordion_arrow.png";
+
+
+
 
 const Accordion = ({ title, content }) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleAccordion = () => {
-        setIsOpen((prev) => !prev);
-    };
-
     return (
-        <div className='accordion'>
+        <div className='accordion fixed'>
             <div className='accordion-header'>
                 <h3>{title}</h3>
-                <div className={`arrow ${isOpen ? 'open' : ''}`} 
-                style={{ backgroundImage: `url(${arrowIcon})`}}
-                onClick={toggleAccordion}></div>
             </div>
-            <div className={`accordion-content ${isOpen ? 'open' : ''}`}>{content}</div>
+            <div className='accordion-content open'>{content}</div>
         </div>
     );
 };

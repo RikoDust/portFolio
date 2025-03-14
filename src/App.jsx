b1549ac39e9de24223ad6e75b3e3f92d2eed568b
Emeric <emeric.pataut@gmail.com>
2025-03-14 17:10:27 +0100

    Add meta tags for SEO optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+
+import { Helmet } from 'react-helmet';
 import RoutesComponent from './RoutesComponent';
-import './styles/main.css'
+import './styles/main.css';
+
+
 
 
 function App() {
   return (
-      <div>
-          <RoutesComponent />
-      </div>
+    <div>
+      {/* Balises meta globales */}
+      <Helmet>
+        <title>Portfolio Emeric Pataut</title>
+        <meta name="description" content="Portfolio d'Emeric Pataut, développeur web. Découvrez mes projets et compétences." />
+        <meta name="keywords" content="Portfolio, Emeric Pataut, Développeur web, projets web" />
+        <meta name="author" content="Emeric Pataut" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
+      
+
+
+      {/* Composant des routes */}
+      <RoutesComponent />
+    </div>
   );
 }
 
 
 
 
-export default App
+export default App;

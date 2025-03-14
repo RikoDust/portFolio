137661023ecd87dbb96a42092bddfb8e28fae8d5
Emeric <emeric.pataut@gmail.com>
2025-03-14 15:27:03 +0100

    feat(home): adapt layout for mobile

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -13,15 +13,10 @@ const Presentation = () => {
 
         <article className="text-presentation">
             <h3>Je m&apos;appelle Emeric et je suis passionné par le développement web ! </h3>
-            <p>Curieux et toujours en quete de nouvelles connaissances, j&apos;aime explorer les technologies pour relever des defis techniques </p>
+            <p>Curieux et toujours en quête de nouvelles connaissances, j&apos;aime explorer les technologies pour relever des defis techniques </p>
             <p>Doté d&apos;un esprit logique et analytique, je conçois des applications modernes, performantes et accesssibles </p>
             <p>Mon objectif ? </p>
             <p>Offrir des expériences web intuitives et optimisées, alliant esthétisme et efficacité </p>
-        </article>
-
-        
-        <article>
-
         </article>
 
         <a

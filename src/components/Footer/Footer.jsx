94f2d888df7f7069cf6921694248b63c97d72482
Emeric <emeric.pataut@gmail.com>
2025-04-02 15:25:26 +0200

    fix: added aria-label to social media links in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
     return (
         <footer className='footer'>
             <div className="liens_reseaux" >
-                <a href="https://www.linkedin.com/in/emeric-pataut/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/emeric-pataut/" target="_blank" rel="noopener noreferrer" aria-label="Profil LinkedIn">
                     <i className="fa-brands fa-linkedin"></i>
                 </a>
-                <a href="https://github.com/RikoDust" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/RikoDust" target="_blank" rel="noopener noreferrer" aria-label="Profil GitHub">
                     <i className="fa-brands fa-square-github"></i>
                 </a>
             </div>
